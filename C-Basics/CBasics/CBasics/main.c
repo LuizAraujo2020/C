042c5201fb1081f3148465f41a8b4515f045be02
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-03-15 18:21:55 -0300

    init project - basics

---
+++ C-Basics/CBasics/CBasics/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CBasics
+//
+//  Created by Luiz Araujo on 15/03/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
