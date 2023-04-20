b2662ac464b17d3253f7d033f634d6c0e0c5740a
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-04-20 20:11:28 -0300

    asd

---
+++ C-Basics/CBasics-GCD/CBasics-GCD/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CBasics-GCD
+//
+//  Created by Luiz Araujo on 15/03/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
